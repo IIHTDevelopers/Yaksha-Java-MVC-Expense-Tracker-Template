61bae006bbd7727eea94ca656e434e3ad94689c7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-02 13:35:12 +0530

    Delete src directory

--- src/main/java/com/yaksha/training/expense/repository/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yaksha.training.expense.repository;
-
-import com.yaksha.training.expense.entity.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
